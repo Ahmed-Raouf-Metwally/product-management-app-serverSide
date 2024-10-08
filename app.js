ee0e6d64310ee0b7b5a1b582190a77211f3433fc
Ahmed Raouf <info@codepressp.net>
2024-10-08 03:26:32 +0300

    vercel config

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
-const express = require('express')
+const express = require( 'express' )
 const app = express()
-require("dotenv").config();
-const connectDB = require('./db/connect')
-var mongoose = require('mongoose');
-const productsRouter = require('./routes/product.routes')
-const userRouter = require('./routes/user.routes')
-const cookieParser = require('cookie-parser')
-const path = require('node:path')
+require( "dotenv" ).config();
+const connectDB = require( './db/connect' )
+var mongoose = require( 'mongoose' );
+const cookieParser = require( 'cookie-parser' )
+const path = require( 'node:path' )
+const { use } = require( 'bcrypt/promises' );
 const cors = require( 'cors' );
 const xss = require( 'xss-clean' );
 const helmet = require( 'helmet' );
-const { use } = require( 'bcrypt/promises' );
-
+const productsRouter = require( './routes/product.routes' )
+const userRouter = require( './routes/user.routes' )
 
 //middlewares
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname,"")))
-
-app.use(cors());
-app.use(xss());
-app.use(helmet());
+app.use( express.json() )
+app.use( cookieParser() )
+app.use( express.static( path.join( __dirname, "" ) ) )
+app.use( cors() );
+app.use( xss() );
+app.use( helmet() );
 
 //routes
-
-app.use('/api/v1/products', productsRouter)
+app.use( '/api/v1/products', productsRouter )
 app.use( '/api/v1/users', userRouter )
 
-
-//connect to databaseexpress. and start server
+//connect to database and start server
 const start = ( async () =>
 {
     const port = process.env.PORT
-    const uri = process.env.MONGO_URI ;
-    try {
-        
+    const uri = process.env.MONGO_URI;
+    try
+    {
         //connect DB
-        await connectDB(uri)
+        await connectDB( uri )
 
         //start server
-        app.listen(port, console.log(`Server is listening on port ${port}...`))
-        
-    } catch (error) {
-        console.log(error)
+        app.listen( port, console.log( `Server is listening on port ${ port }...` ) )
+
+    } catch ( error )
+    {
+        console.log( error )
     }
 
-})()
+} )()
